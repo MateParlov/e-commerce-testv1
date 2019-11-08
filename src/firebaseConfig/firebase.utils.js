ca96c4522c3be16c9606bf3101cc447752749544
Tomislav Sertic <tsertic5@gmail.com>
2019-11-08 11:41:00 +0100

    firebase config update

---
+++ src/firebaseConfig/firebase.utils.js
@@ -0,0 +1,48 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyAJZEU_UtdvDjin5bs2GFdxFYnIkjaH7pM',
+  authDomain: 'ecommercesite-29055.firebaseapp.com',
+  databaseURL: 'https://ecommercesite-29055.firebaseio.com',
+  projectId: 'ecommercesite-29055',
+  storageBucket: 'ecommercesite-29055.appspot.com',
+  messagingSenderId: '185745907569',
+  appId: '1:185745907569:web:e432579c5af64ec1dde0d0'
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
